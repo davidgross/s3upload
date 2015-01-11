90913111c9462ba3ec4b5e0c13de07d312de5007
David Gross <david.gross@daggerweb.org>
2015-01-11 13:04:53 -0700

    fixed patches for 2.7.3

--- include/patcher_patch.inc.php
+++ include/patcher_patch.inc.php
@@ -10,11 +10,11 @@ function s3upload_patcher_patch(){
 		$_SESSION['page_errors'][] = l10n('S3Upload Plugin failed to patch \'admin/include/functions_upload.inc.php\' file (probably a file READ access permission issue).');
 		$result = 'Failed';
 	}else{
-		if(md5($file_contents) == 'a288c1bbae4f25cb03b9e6db61e8494b'){ //File contents hash as of Piwigo 2.5.0
+		if(md5($file_contents) == '429a26cf5d62978e8bfb6ef6ff7c3622'){ //File contents hash as of Piwigo 2.7.3
 			$file_contents = str_replace('move_uploaded_file($source_filepath, $file_path);','move_uploaded_file($source_filepath, $file_path); copy($file_path, $file_path.\'.s3\'); //patch by s3Upload Plugin', $file_contents);
 			@file_put_contents($path_to_functions_upload, $file_contents);
 			$file_contents = @file_get_contents($path_to_functions_upload);
-			if( ($file_contents === false) || (md5($file_contents) != '38dc62b3623ece4adda679e04a943f75') ){ //File contents after patching
+			if( ($file_contents === false) || (md5($file_contents) != 'fa6c8c2d36586b1a53e6f7a608f5bab5') ){ //File contents after patching
 				$result = 'Failed';
 				$_SESSION['page_errors'][] = l10n('S3Upload Plugin failed to patch \'admin/include/functions_upload.inc.php\' file (probably a file WRITE access permission issue).') . ' ' . l10n('So, S3 uploads will be the Piwigo-reduced size (if applicable), not the true original.');
 			}
